e3acdfd16ad11733ca3724c1c599439c0be91bd6
octopus <912554887@qq.com>
2019-06-17 00:24:15 +0800

    add Transaction Receipt print

--- src/main/java/org/fisco/bcos/abi/encoder/callback/CallBackUtils.java
+++ src/main/java/org/fisco/bcos/abi/encoder/callback/CallBackUtils.java
@@ -7,26 +7,88 @@ import org.fisco.bcos.web3j.protocol.exceptions.TransactionException;
 import org.fisco.bcos.web3j.tx.txdecode.BaseException;
 import org.fisco.bcos.web3j.tx.txdecode.TransactionDecoder;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public class CallBackUtils {
 	
 	public static boolean openCallBack = true;
 	
-	public static void txDecodeInCallBack(TransactionDecoder decode, TransactionReceipt receipt) throws BaseException, TransactionException, IOException {
+	public static void txDecodeInCallBack(TransactionDecoder decode, TransactionReceipt receipt) {
 		
 		if(openCallBack) {
 			System.out.println(" receipt => " + receipt);
-			System.out.println(" input json => ");
-			System.out.println("\t " + decode.decodeInputReturnJson(receipt.getInput()));
-			System.out.println(" input object => ");
-			System.out.println("\t " + decode.decodeInputReturnObject(receipt.getInput()));
-			System.out.println(" output json => ");
-			System.out.println("\t " + decode.decodeOutputReturnJson(receipt.getInput(), receipt.getOutput()));
-			System.out.println(" output object => ");
-			System.out.println("\t " + decode.decodeOutputReturnObject(receipt.getInput(), receipt.getOutput()));
-			System.out.println(" event json => ");
-			System.out.println("\t " + decode.decodeEventReturnJson(receipt.getLogs()));
-			System.out.println(" event object => ");
-			System.out.println("\t " + decode.decodeEventReturnObject(receipt.getLogs()));
+			
+			try {
+				System.out.println(" input json => ");
+				System.out.println("\t " + decode.decodeInputReturnJson(receipt.getInput()));
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransactionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (BaseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				System.out.println(" input object => ");
+				System.out.println("\t " + decode.decodeInputReturnObject(receipt.getInput()));
+			} catch (BaseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransactionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				System.out.println(" output json => ");
+				System.out.println("\t " + decode.decodeOutputReturnJson(receipt.getInput(), receipt.getOutput()));
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (BaseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransactionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				System.out.println(" output object => ");
+				System.out.println("\t " + decode.decodeOutputReturnObject(receipt.getInput(), receipt.getOutput()));
+			} catch (TransactionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (BaseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				System.out.println(" event json => ");
+				System.out.println("\t " + decode.decodeEventReturnJson(receipt.getLogs()));
+			} catch (BaseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				System.out.println(" event object => ");
+				System.out.println("\t " + decode.decodeEventReturnObject(receipt.getLogs()));
+			} catch (BaseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
